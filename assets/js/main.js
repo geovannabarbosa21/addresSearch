16ff524cc27841753a191d2b7938e5fd11450a2b
Geovanna <s.b.geovanna.99@gmail.com>
2023-05-18 22:31:11 -0300

    Desafio de Desenvolvimento

---
+++ assets/js/main.js
@@ -0,0 +1,146 @@
+
+(function() {
+  "use strict";
+
+  /**
+   * 
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * 
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+
+  /**
+   * Scroll with offset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+  /**
+   * Initiate Pure Counter 
+   */
+  new PureCounter();
+
+})()
+
+function buscarCep(value){
+
+
+  document.getElementById('cep').value = value.replace(/[^0-9]/g, '')
+  
+   if (value.length !== 8) {
+    return;
+}
+
+let url = `https://viacep.com.br/ws/${value}/json/`;
+
+fetch(url, {
+  })
+  .then(response => response.json())
+  .then(data => {
+    // do anithing with response 
+
+
+    console.log(data)
+    if(data.erro){
+      alert('Não foi possivel encontrar esse cep 😟')
+      return
+    }
+    document.getElementById('cidade').value = data.localidade
+    document.getElementById('bairro').value = data.bairro
+    document.getElementById('endereco').value = data.logradouro
+    document.getElementById('sigla').value = data.uf
+
+  })
+  .catch(error => {
+    // erros here
+    console.error(error);
+  });
+
+}
+
+
+
+
